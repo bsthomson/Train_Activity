60a89ec7dff81fbc842f34ec83c96a44e85cf639
bsthomson <thomson.bs@gmail.com>
2018-06-26 07:58:14 -0500

    meat and potatoes

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyAszfGZSOd6z-3vEEEJ3oD_PkM7gYaEobk",
+    authDomain: "dog-pit.firebaseapp.com",
+    databaseURL: "https://dog-pit.firebaseio.com",
+    projectId: "dog-pit",
+    storageBucket: "dog-pit.appspot.com",
+    messagingSenderId: "560316186785"
+  };
+  firebase.initializeApp(config);
