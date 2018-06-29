1e854f53b009705fed128116c8c09366c74f3e7a
bsthomson <thomson.bs@gmail.com>
2018-06-28 23:38:03 -0500

    changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+// link to firebase
 var config = {
   apiKey: "AIzaSyAszfGZSOd6z-3vEEEJ3oD_PkM7gYaEobk",
   authDomain: "dog-pit.firebaseapp.com",
@@ -10,19 +11,17 @@ firebase.initializeApp(config);
   
 var database = firebase.database();
 
+// on click effect for submit button
 $("#submit").on("click", function(event) {
   event.preventDefault();
   
+  // saves form values as variables
   var trainName = $("#train-name").val().trim();
   var trainDestination = $("#destination").val().trim();
   var trainFirstTime = moment($("#first-time").val().trim(), 'HH:mm').format('HH:mm');
   var trainFrequency = $("#frequency").val().trim();
 
-  // console.log(trainName);
-  // console.log(trainDestination);
-  // console.log(trainFirstTime);
-  // console.log(trainFrequency);
-
+  // saves variable in an object
   var newTrain = {
     train: trainName,
     destination: trainDestination,
@@ -30,13 +29,10 @@ $("#submit").on("click", function(event) {
     frequency: trainFrequency,
   };
 
+  // pushes object to database
   database.ref().push(newTrain);
 
-  // console.log(newTrain.train);
-  // console.log(newTrain.destination);
-  // console.log(newTrain.initialtime);
-  // console.log(newTrain.frequency);
-
+  // clears out form
   $("#train-name").val("");
   $("#destination").val("");
   $("#first-time").val("");
@@ -44,49 +40,38 @@ $("#submit").on("click", function(event) {
 
 });
 
+// when database changes
 database.ref().on("child_added", function(childSnapshot, prevChildKey) {
-  // console.log(childSnapshot.val());
 
+  // saves snapshots of database as variables
   var ctrainName = childSnapshot.val().train;
   var ctrainDestination = childSnapshot.val().destination;
   var ctrainFirstTime = childSnapshot.val().initialtime;
   var ctrainFrequency = childSnapshot.val().frequency;
-  var updatedTime;
-  console.log(moment(ctrainFirstTime, "HH:mm").format("HH:mm"));
-
-  // console.log(ctrainName);
-  // console.log(ctrainDestination);
-  // console.log(ctrainFirstTime);
-  // console.log(ctrainFrequency);
-
+  var updatedTime = ctrainFirstTime;
   
-  var cnextArrival = moment(updatedTime, 'HH:mm').add(ctrainFrequency, 'm').format('HH:mm');
-  if (Math.sign(parseInt(moment().diff(moment(ctrainFirstTime, 'HH:mm'), 'm'))) === -1) {
-    var updatedTime = (moment(ctrainFirstTime, 'HH:mm').add(24, 'h'));
-  }
-  else if (parseInt(moment().diff(moment(ctrainFirstTime, 'HH:mm'), 'm')) > parseInt(ctrainFrequency)) {
-    var updatedTime = moment(ctrainFirstTime, 'HH:mm').add(parseInt(ctrainFrequency), 'm');
-    console.log(moment(ctrainFirstTime, 'HH:mm').add(parseInt(ctrainFrequency), 'm'));
-    console.log(moment(updatedTime, "HH:mm").format('HH:mm'));
-    console.log(parseInt(ctrainFrequency));
-    console.log(moment(ctrainFirstTime, "HH:mm").format("HH:mm"));
-  }
-  else {
-    var updatedTime = ctrainFirstTime;
+  // if the difference between the current time and updatedTime is negative
+  if (Math.sign(parseInt(moment().diff(moment(updatedTime, 'HH:mm'), 'm'))) === -1) {
+    // adds 24 hours to the updatedTime to keep it positve
+    var updatedTime = (moment(updatedTime, 'HH:mm').add(24, 'h'));
   };
-  
-  console.log(moment().diff(moment(ctrainFirstTime, 'HH:mm'), 'm'));
-  console.log(parseInt(moment().diff(moment(ctrainFirstTime, 'HH:mm'), 'm')) > parseInt(ctrainFrequency));
 
+  // while the difference of the current time and updated time in minutes is greater than the frequency
+  while (parseInt(moment().diff(moment(updatedTime, 'HH:mm'), 'm')) > parseInt(ctrainFrequency)) {
+    // add the frequency in minutes to the updatedTime until the the difference is less than the frequence
+    var updatedTime = moment(updatedTime, 'HH:mm').add(ctrainFrequency, 'm');
+  };
+  // variable for the next arrival. adding the frequency to the updatedTime
+  var cnextArrival = moment(updatedTime, 'HH:mm').add(ctrainFrequency, 'm').format('HH:mm');
+  //variable for the minutes away. difference between the next arrival and the current time
   var cminutesAway = moment(cnextArrival, 'HH:mm').diff(moment(), "minutes");
   
+  // if the minutes away is negative, add 24 hours to the next arrival to keep the time positive
   if (Math.sign(cminutesAway) === -1) {
     var cminutesAway = (moment(cnextArrival, 'HH:mm').add(24, 'h')).diff(moment(), "minutes");
   };
-
-  // console.log(cminutesAway);
   
-
+  // appends the table with train information
   $("#tablebody").append(`
     <tr>
       <th scope='row'>${ctrainName}</th>
